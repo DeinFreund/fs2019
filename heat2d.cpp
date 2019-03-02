aa58777b72fa2235a10f1bf045f627916851b355
DeinFreund <deinbestrfreund@gmail.com>
2019-03-02 15:36:25 +0100

    hybrid

--- heat2d.cpp
+++ heat2d.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 using numeric=long ;
 using numericF= long;
+#define debug 0
 
 class GridLevel
 {
@@ -47,7 +48,7 @@ numeric logNum(numeric in){
 
 numeric convertU(double in, int l){
   numeric ret = ((numeric)(in * (1LL << shifts[l])));
-  //cerr << in << " -> " << ret << " ";
+  //if (debug) cerr << in << " -> " << ret << " ";
   return ret;
 }
 
@@ -56,7 +57,7 @@ double revertF(numericF in, int l){
 }
 numericF convertF(double in, int l){
   numericF ret = ((numericF)(in * (1LL << (shifts[l] - fReduction[l]))));
-  //cerr << in << " -> " << ret << " ";
+  //if (debug) cerr << in << " -> " << ret << " ";
   return ret;
 }
 
@@ -74,16 +75,16 @@ void printMinMax(GridLevel * g){
 	minv = min(revertU(g[l].U[i][j], l), minv);
 	maxv = max(revertU(g[l].U[i][j], l), maxv);
       }
-    cerr << "= Level " << l << " =" << endl;
-    cerr << "minu: " << minv << endl;
-    cerr << "maxu: " << maxv << endl;
+    if (debug) cerr << "= Level " << l << " =" << endl;
+    if (debug) cerr << "minu: " << minv << endl;
+    if (debug) cerr << "maxu: " << maxv << endl;
     for (int j = 0; j < g[l].N; j++)
       for (int i = 0; i < g[l].N; i++){
 	minv = min(revertF(g[l].f[i][j],l), minv);
 	maxv = max(revertF(g[l].f[i][j],l), maxv);
       }
-    cerr << "minf: " << minv << endl;
-    cerr << "maxf: " << maxv << endl;
+    if (debug) cerr << "minf: " << minv << endl;
+    if (debug) cerr << "maxf: " << maxv << endl;
   }
 }
 
@@ -96,7 +97,7 @@ int getInitialShift(GridLevel * g, Heat2DSetup& s){
       maxv = max(abs(s.getRHS(i,j)), maxv);
     }
   int newFShift = 30 - log(maxv)/log(2);
-  cerr << "largest F/Res is " << maxv << " use " << newFShift << " F shift" << endl;
+  if (debug) cerr << "largest F/Res is " << maxv << " use " << newFShift << " F shift" << endl;
   return newFShift;
 }
 
@@ -114,11 +115,11 @@ void adjustShift(GridLevel * g, int grids){
 	minv = min(revertU(g[l].U[i][j],l), minv);
 	maxv = max(abs(revertU(g[l].U[i][j],l)), maxv);
       }
-    cerr << "= Level " << l << " =" << endl;
-    cerr << "minu: " << minv << endl;
-    cerr << "maxu: " << maxv << endl;
+    if (debug) cerr << "= Level " << l << " =" << endl;
+    if (debug) cerr << "minu: " << minv << endl;
+    if (debug) cerr << "maxu: " << maxv << endl;
     int newUShift = 30 - log(maxv)/log(2);
-    cerr << "largest U is " << maxv << " use " << newUShift << " shift" << endl;
+    if (debug) cerr << "largest U is " << maxv << " use " << newUShift << " shift" << endl;
     
     minv = 1e8;
     maxv = 1;
@@ -127,10 +128,10 @@ void adjustShift(GridLevel * g, int grids){
 	minv = min(revertF(g[l].f[i][j],l), minv);
 	maxv = max(abs(revertF(g[l].f[i][j],l)), maxv);
       }
-    cerr << "minf: " << minv << endl;
-    cerr << "maxf: " << maxv << endl;
+    if (debug) cerr << "minf: " << minv << endl;
+    if (debug) cerr << "maxf: " << maxv << endl;
     int newUShift2 = 30 - log(maxv/(g[l].h * g[l].h))/log(2);
-    cerr << "largest F is " << maxv << " use " << newUShift2 << " shift" << endl;
+    if (debug) cerr << "largest F is " << maxv << " use " << newUShift2 << " shift" << endl;
 
     bestShift = min(bestShift, min(newUShift2, newUShift));
 
@@ -142,11 +143,11 @@ void adjustShift(GridLevel * g, int grids){
 	minv = min(revertF(g[l].Res[i][j],l), minv);
 	maxv = max(abs(revertF(g[l].Res[i][j],l)), maxv);
       }
-    cerr << "minres: " << minv << endl;
-    cerr << "maxres: " << maxv << endl;
+    if (debug) cerr << "minres: " << minv << endl;
+    if (debug) cerr << "maxres: " << maxv << endl;
     maxv = max(maxv, maxf);
     int newFShift = 30 - log(maxv)/log(2);
-    cerr << "largest F/Res is " << maxv << " use " << newFShift << " F shift" << endl;
+    if (debug) cerr << "largest F/Res is " << maxv << " use " << newFShift << " F shift" << endl;
   }
 
   int newUShift = bestShift;
@@ -155,7 +156,7 @@ void adjustShift(GridLevel * g, int grids){
     //newUShift = 27;
     lowerUShift = newUShift;
     int extraShift = min(1,max(0, newUShift - shifts[l]));
-    cerr << "new U shift is " << shifts[l] + extraShift << " change: " << extraShift << endl;
+    if (debug) cerr << "new U shift is " << shifts[l] + extraShift << " change: " << extraShift << endl;
     shifts[l] = shifts[l] + extraShift;
     fReduction[l] += extraShift;
     for (int j = 0; j < g[l].N; j++)
@@ -171,7 +172,7 @@ void adjustShift(GridLevel * g, int grids){
 	 extraShift = 0;
 	 fReduction[l] -= extraShift;
 
-	 cerr << "new F shift is " << newFShift << " ( " << fReduction[l] << ")" << endl;    
+	 if (debug) cerr << "new F shift is " << newFShift << " ( " << fReduction[l] << ")" << endl;    
 	 for (int j = 0; j < g[l].N; j++)
 	 {
 	 for (int i = 0; i < g[l].N; i++){ //wtf really needed?
@@ -226,6 +227,7 @@ void heat2DSolver(Heat2DSetup& s)
       g[i].dRes = alloc2D<double>(g[i].N, g[i].N);
       g[i].df   = alloc2D<double>(g[i].N, g[i].N);
     }
+  numeric** bestU = alloc2D<numeric>(g[0].N, g[0].N);;
   shifts = (int*)calloc(sizeof(int), s.gridCount);
   fReduction = (int*)calloc(sizeof(int), s.gridCount);
 
@@ -238,66 +240,104 @@ void heat2DSolver(Heat2DSetup& s)
   // Setting up problem.
   for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].U[i][j] = convertU(s.getInitial(i,j),0);
   for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].f[i][j] = convertF(s.getRHS(i,j),0);
-  for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].df[i][j] = (s.getRHS(i,j),0);
+  for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].df[i][j] = (s.getRHS(i,j));
+    for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].dU[i][j] = s.getInitial(i,j);
 
+  highPrecMode = 0;
   //adjustShift(g, s.gridCount);
-  int blub = 0;
-  double minDiff = s.L2NormDiff;
-  double prevDiff = s.L2NormDiff;
-  int divergesCount = 0;
-  
-  while (s.L2NormDiff > 1e-2)  // Multigrid solver start
-    {
-      minDiff = min(s.L2NormDiff, minDiff);
-      if (s.L2NormDiff > 2*minDiff && s.L2NormDiff > prevDiff){
-	divergesCount ++;//diverging, numeric limit reached
-      }else if (s.L2NormDiff < 1e-7 + minDiff){
-	divergesCount = 0;
-      }
-      else
-	{
-	  divergesCount = max(0, divergesCount - 1);
-	}
-      if (divergesCount > 50){
-	cerr << "Solver is no longer converging. The best norm difference was " << minDiff << endl;
-	break;
-      }
-      prevDiff = s.L2NormDiff;
-      s.applyJacobi_(g, 0, s.downRelaxations); // Relaxing the finest grid first
-      s.calculateResidual_(g, 0); // Calculating Initial Residual
 
-      for (int grid = 1; grid < s.gridCount; grid++) // Going down the V-Cycle
-	{
-	  s.applyRestriction_(g, grid); // Restricting the residual to the coarser grid's solution vector (f)
-	  s.applyJacobi_(g, grid, s.downRelaxations); // Smoothing coarser level
-	  s.calculateResidual_(g, grid); // Calculating Coarse Grid Residual
+  for (int mode = 0; mode <2 ; mode++){
+    
+  int blub = 0;
+    //s.calculateL2Norm_(g, 0); // Calculating Residual L2 Norm
+    highPrecMode = mode;
+    double minDiff = s.L2NormDiff;
+    double prevDiff = s.L2NormDiff;
+    int divergesCount = 0;
+
+    if (mode == 1){
+      highPrecMode = 1;
+      s.calculateResidual_(g, 0); // Calculating Initial Residual
+	s.calculateL2Norm_(g, 0); // Calculating Residual L2 Norm
+	  cout << "init diff: " << s.L2Norm << endl;
+    }
+    s.L2NormDiff = 1e100;
+    s.L2Norm = 1e100;
+    
+    while (s.L2NormDiff > (highPrecMode ? s.tolerance : s.tolerance))  // Multigrid solver start
+      {
+	if (s.L2Norm < minDiff){
+	  minDiff =s.L2Norm;
+	  //cerr << "The new best norm is " << minDiff << endl;
+	  if (mode == 0)for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].dU[i][j] = revertU(g[0].U[i][j],0);
+	  if (mode == 0)for (int i = 0; i < s.N; i++) for (int j = 0; j < s.N; j++) g[0].dRes[i][j] = revertF(g[0].Res[i][j],0);
 	}
+	if (s.L2NormDiff > 2*minDiff && s.L2NormDiff > prevDiff){
+	  divergesCount ++;//diverging, numeric limit reached
+	}else if (s.L2NormDiff < 1e-7 + minDiff){
+	  divergesCount = 0;
+	}
+	else
+	  {
+	    divergesCount = max(0, divergesCount - 1);
+	  }
+	
+	prevDiff = s.L2Norm;
+	if (divergesCount > 10){
+	   cerr << "Solver is no longer converging. The best norm difference was " << minDiff << endl;
+	  break;
+	}
+	s.applyJacobi_(g, 0, s.downRelaxations); // Relaxing the finest grid first
+	s.calculateResidual_(g, 0); // Calculating Initial Residual
+
+	for (int grid = 1; grid < s.gridCount; grid++) // Going down the V-Cycle
+	  {
+	    s.applyRestriction_(g, grid); // Restricting the residual to the coarser grid's solution vector (f)
+	    s.applyJacobi_(g, grid, s.downRelaxations); // Smoothing coarser level
+	    s.calculateResidual_(g, grid); // Calculating Coarse Grid Residual
+	  }
       
 
-      for (int grid = s.gridCount-1; grid > 0; grid--) // Going up the V-Cycle
-	{
-	  s.applyProlongation_(g, grid); // Prolonging solution for coarser level up to finer level
-	  s.applyJacobi_(g, grid, s.upRelaxations); // Smoothing finer level
+	for (int grid = s.gridCount-1; grid > 0; grid--) // Going up the V-Cycle
+	  {
+	    s.applyProlongation_(g, grid); // Prolonging solution for coarser level up to finer level
+	    s.applyJacobi_(g, grid, s.upRelaxations); // Smoothing finer level
+	  }
+	
+	//       printMinMax(g);
+	s.calculateResidual_(g, 0); // Calculating Initial Residual
+	s.calculateL2Norm_(g, 0); // Calculating Residual L2 Norm
+	if (blub++ % 50 == 0 || 0){
+	  cout << "diff: " << s.L2NormDiff << endl;
+	  cout << "norm: " << s.L2Norm << endl;
+	  //cout << "mindiff: " << minDiff << endl;
+
+	  if (mode == 0)adjustShift(g, s.gridCount);
 	}
-      if (blub++ % 50 == 0){
-	cout << "diff: " << s.L2NormDiff << endl;
-
-	adjustShift(g, s.gridCount);
-      }
-      //       printMinMax(g);
-      s.calculateL2Norm_(g, 0); // Calculating Residual L2 Norm
-    }  // Multigrid solver end
-
-  cerr << "The last norm difference was " << s.L2NormDiff << endl;
+      }  // Multigrid solver end
+    
+    cerr << "The last norm difference was " << s.L2NormDiff << " (norm "  << s.L2Norm<< ") with " << shifts[0] << " shifts" << endl;
+  }
   // Saving solution before returning
-  for (int i = 0; i < g[0].N; i++) for (int j = 0; j < g[0].N; j++) s.saveSolution(i, j, revertU(g[0].U[i][j], 0));
+  for (int i = 0; i < g[0].N; i++) for (int j = 0; j < g[0].N; j++) s.saveSolution(i, j, g[0].dU[i][j]);
 }
 
 
 void applyJacobi(GridLevel* g, int l, int relaxations)
 {
   if (highPrecMode){
+    for (int r = 0; r < relaxations; r++)
+      {
+	swap(g[l].dUn, g[l].dU);
 
+	double fac = pow(g[l].dh,2)/4;
+	int N = g[l].N-1;
+	for (int x = 1; x < N; x += blockWidth)
+	  for (int y = 1; y < N; y += blockHeight)
+	    for (int i = x; i < min(N, x + blockWidth); i++)
+	      for (int j = y; j < min (N, y + blockHeight); j++) // Perform a Jacobi Iteration
+		g[l].dU[i][j] = (g[l].dUn[i-1][j] + g[l].dUn[i+1][j] + g[l].dUn[i][j-1] + g[l].dUn[i][j+1])* 0.25 + g[l].df[i][j]*fac;
+      }
   }
   else{
     for (int r = 0; r < relaxations; r++)
@@ -321,7 +361,13 @@ void calculateResidual(GridLevel* g, int l)
 {
   
   if (highPrecMode){
-
+    int N = g[l].N-1;
+    double fac = 1 / (pow(g[l].dh,2));
+    for (int x = 1; x < N; x += blockWidth)
+      for (int y = 1; y < N; y += blockHeight)
+	for (int i = x; i < min(N, x + blockWidth); i++)
+	  for (int j = y; j < min (N, y + blockHeight); j++) 
+	    g[l].dRes[i][j] = g[l].df[i][j] + (g[l].dU[i-1][j] + g[l].dU[i+1][j] - 4*g[l].dU[i][j] + g[l].dU[i][j-1] + g[l].dU[i][j+1]) * fac;
   }
   else{
     int N = g[l].N-1;
@@ -338,7 +384,13 @@ void calculateResidual(GridLevel* g, int l)
 double calculateL2Norm(GridLevel* g, int l)
 {
   if (highPrecMode){
+    int N = g[l].N-1;
+    double tmp = 0.0;
+    for (int i = 0; i < g[l].N; i++)
+      for (int j = 0; j < g[l].N; j++)
+	tmp += g[l].dRes[i][j]*g[l].dRes[i][j];
 
+    return sqrt(tmp);
   }
   else{
     int N = g[l].N-1;
@@ -347,7 +399,7 @@ double calculateL2Norm(GridLevel* g, int l)
     for (int i = 0; i < g[l].N; i++)
       for (int j = 0; j < g[l].N; j++){
 	tmp += intSquare(g[l].Res[i][j] << s, l);
-	//cerr << g[l].Res[i][j] << " | " << intSquare(g[l].Res[i][j]) << " | " << tmp << endl;
+	//if (debug) cerr << g[l].Res[i][j] << " | " << intSquare(g[l].Res[i][j]) << " | " << tmp << endl;
       }
 
     return sqrt((tmp) / (double)(1 << (s << 1)));
@@ -358,7 +410,18 @@ void applyRestriction(GridLevel* g, int l)
 {
   
   if (highPrecMode){
+    int N = g[l].N-1;
+    for (int x = 1; x < N; x += blockWidth)
+      for (int y = 1; y < N; y += blockHeight)
+	for (int i = x; i < min(N, x + blockWidth); i++)
+	  for (int j = y; j < min (N, y + blockHeight); j++) 
+	    g[l].df[i][j] = ( 1.0*( g[l-1].dRes[2*i-1][2*j-1] + g[l-1].dRes[2*i-1][2*j+1] + g[l-1].dRes[2*i+1][2*j-1]   + g[l-1].dRes[2*i+1][2*j+1] ) +
+			      2.0*( g[l-1].dRes[2*i-1][2*j]   + g[l-1].dRes[2*i][2*j-1]   + g[l-1].dRes[2*i+1][2*j]     + g[l-1].dRes[2*i][2*j+1]   ) +
+			      4.0*( g[l-1].dRes[2*i][2*j] ) ) * 0.0625;
 
+    for (int i = 0; i < g[l].N; i++)
+      for (int j = 0; j < g[l].N; j++)
+	g[l].dU[i][j] = 0;
   }
   else{
     int shift = (shifts[l] - fReduction[l]) - (shifts[l-1] - fReduction[l-1]);
@@ -381,7 +444,25 @@ void applyProlongation(GridLevel* g, int l)
 {
   
   if (highPrecMode){
+    for (int j = 1; j < g[l].N-1; j++)
+      for (int i = 1; i < g[l].N-1; i++)
+	g[l-1].dUn[2*i][2*j] = g[l].dU[i][j];
 
+    for (int j = 1; j < g[l].N-1; j++)
+      for (int i = 1; i < g[l].N; i++)
+	g[l-1].dUn[2*i-1][2*j] = ( g[l].dU[i-1][j] + g[l].dU[i][j] ) * 0.5;
+
+    for (int j = 1; j < g[l].N; j++)
+      for (int i = 1; i < g[l].N-1; i++)
+	g[l-1].dUn[2*i][2*j-1] = ( g[l].dU[i][j-1] + g[l].dU[i][j] )  * 0.5;
+
+    for (int j = 1; j < g[l].N; j++)
+      for (int i = 1; i < g[l].N; i++)
+	g[l-1].dUn[2*i-1][2*j-1] = ( g[l].dU[i-1][j-1] + g[l].dU[i-1][j] + g[l].dU[i][j-1] + g[l].dU[i][j] ) * 0.25;
+
+    for (int j = 0; j < g[l-1].N; j++)
+      for (int i = 0; i < g[l-1].N; i++)
+	g[l-1].dU[i][j] += g[l-1].dUn[i][j];
   }
   else{
     int shift = shifts[l] - shifts[l-1];
